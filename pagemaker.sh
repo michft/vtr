dffe1e12332ee3bc27bcf156c24485e1afe40dc8
Michael Tomkins <michft@gmail.com>
2013-09-07 23:28:57 +0000

    There shall be only one, download.

--- pagemaker.sh
+++ pagemaker.sh
@@ -4,10 +4,9 @@
 cat in > /usr/share/nginx/www/vtr.html
 
 wget -O - http://vtr.aec.gov.au/Default.htm |  sed -n '/.*HousePartyRepresentationChart.*/{n;p;n;p;n;p;n;p;n;p;n;p;n;p}' | sed "s@.*td.*@@" | sed "s@.*--.*--.*@@"  > /usr/share/nginx/www/vtr.txt
-wget -O - http://vtr.aec.gov.au/Default.htm |  sed -n '/.*HousePartyRepresentationChart.*/{n;p;n;p;n;p;n;p;n;p;n;p;n;p}' | sed "s@.*td.*@@" | sed "s@.*--.*--.*@@"  >> /usr/share/nginx/www/vtr.html
+
+cat  /usr/share/nginx/www/vtr.txt >> /usr/share/nginx/www/vtr.html
 
 echo "</pre></body></html> " >> /usr/share/nginx/www/vtr.html
-chmod 644 /usr/share/nginx/www/vtr.txt
-
 
 exit 0
